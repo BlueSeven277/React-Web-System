8a67d6f361761d8a13f4a3a3a9cc54c6b7a3afad
Rosie(Ruoxi) Shi <shiruoxi1122a@163.com>
2018-12-09 14:52:57 -0500

    store operations

---
+++ it_encyclopedia/src/store/index.js
@@ -0,0 +1,7 @@
+import reducer from "./reducer.js";
+import { createStore, applyMiddleware, compose } from "redux";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers());
+
+export default store;
